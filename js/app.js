d4ad08b84976c8f5187020c5953563d27714ef6f
Eduardo Cruz <eduardo@eduardocruz.com>
2019-02-07 21:21:04 -0300

    Installed Laravel Mix - https://laravel-mix.com/docs/4.0/installation

---
+++ js/app.js
@@ -0,0 +1,38 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["/js/app"],{
+
+/***/ "./src/app.js":
+/*!********************!*\
+  !*** ./src/app.js ***!
+  \********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+
+
+/***/ }),
+
+/***/ "./src/app.scss":
+/*!**********************!*\
+  !*** ./src/app.scss ***!
+  \**********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ 0:
+/*!*****************************************!*\
+  !*** multi ./src/app.js ./src/app.scss ***!
+  \*****************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(/*! /Users/eduardosantos/Code/eduardocruz.github.com/src/app.js */"./src/app.js");
+module.exports = __webpack_require__(/*! /Users/eduardosantos/Code/eduardocruz.github.com/src/app.scss */"./src/app.scss");
+
+
+/***/ })
+
+},[[0,"/js/manifest","/js/vendor"]]]);
